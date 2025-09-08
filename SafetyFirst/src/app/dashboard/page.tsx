8f8712c85589cf73192a3e6fe9fef3aa4934022c
Harsh Ratnaparkhe <79145959+123HARSH456@users.noreply.github.com>
2025-09-08 19:58:21 +0530

    Fixed linting errors

--- SafetyFirst/src/app/dashboard/page.tsx
+++ SafetyFirst/src/app/dashboard/page.tsx
@@ -1,13 +1,4 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+
 
 export default function DashboardPage() {
   return (
